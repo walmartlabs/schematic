3043e8d24593cddbe75df04f30dbc043abe939d6
Howard M. Lewis Ship <hlship@walmartlabs.com>
2018-03-29 13:46:49 -0700

    Setup for CircleCI build

--- project.clj
+++ project.clj
@@ -4,4 +4,9 @@
   :license {:name "Apache Software License 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [com.stuartsierra/component "0.3.2"]])
+                 [com.stuartsierra/component "0.3.2"]]
+  :plugins [[lein-codox "0.10.3"]
+            [test2junit "1.2.5"]]
+  :test2junit-output-dir ~(or (System/getenv "CIRCLE_TEST_REPORTS") "target/test2junit")
+  :codox {:source-uri "https://github.com/walmartlabs/schematic/blob/master/{filepath}#L{line}"
+          :metadata {:doc/format :markdown}})

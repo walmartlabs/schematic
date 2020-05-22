ce38a604fe52de42fc6f6170dbdf35c15a2b8f1c
Howard M. Lewis Ship <hlship@walmartlabs.com>
2020-05-22 10:06:05 -0700

    Update version to 1.3.0
    Update some dependencies

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject com.walmartlabs/schematic "1.2.0"
+(defproject com.walmartlabs/schematic "1.3.0"
   :description "A library to aid in assembling Systems for use with Component"
   :url "https://github.com/walmartlabs/schematic"
   :license {:name "Apache Software License 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [com.stuartsierra/component "0.3.2"]]
-  :plugins [[lein-codox "0.10.3"]
+                 [com.stuartsierra/component "1.0.0"]]
+  :plugins [[lein-codox "0.10.7"]
             [test2junit "1.2.5"]]
   :test2junit-output-dir ~(or (System/getenv "CIRCLE_TEST_REPORTS") "target/test2junit")
   :codox {:source-uri "https://github.com/walmartlabs/schematic/blob/master/{filepath}#L{line}"
